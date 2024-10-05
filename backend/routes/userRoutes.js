c63b7150e37bae9c6fa207baad30b0cc44dd7b1a
Filmzone&Flixify <159073116+1056516@users.noreply.github.com>
2024-10-04 21:18:19 -0400

    Create userRoutes.js

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const User = require('../models/User');
+const router = express.Router();
+
+router.post('/register', async (req, res) => {
+    const { username, password } = req.body;
+    const newUser = new User({ username, password });
+    await newUser.save();
+    res.status(201).json(newUser);
+});
+
+module.exports = router;
